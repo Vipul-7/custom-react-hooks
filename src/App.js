ec141109f1fc9c64b9de5638424ec207dc401175
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-02-07 18:30:37 +0530

    Added a custom hooks logic for http req.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,61 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./Hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://tasks-learning-custom-hooks-default-rtdb.firebaseio.com/Tasks.json'
-      );
+  // const fetchTasks = async (taskText) => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       'https://tasks-learning-custom-hooks-default-rtdb.firebaseio.com/Tasks.json'
+  //     );
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+  //     if (!response.ok) {
+  //       throw new Error('Request failed!');
+  //     }
 
-      const data = await response.json();
+  //     const data = await response.json();
 
+  // const loadedTasks = [];
+
+  // for (const taskKey in data) {
+  //   loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+  // }
+
+  // setTasks(loadedTasks);
+  //   } catch (err) {
+  //     setError(err.message || 'Something went wrong!');
+  //   }
+  //   setIsLoading(false);
+  // };
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    const transformTasks = (taskObj) => {
       const loadedTasks = [];
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
       }
 
       setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+    };
 
-  useEffect(() => {
-    fetchTasks();
+    const requestConfig = {
+      url: "https://tasks-learning-custom-hooks-default-rtdb.firebaseio.com/Tasks.json",
+      method: "GET",
+      body: null,
+      headers: {},
+    };
+    fetchTasks(requestConfig, transformTasks);
   }, []);
 
   const taskAddHandler = (task) => {
